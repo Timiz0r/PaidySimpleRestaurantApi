745b51c446298372256c568935c920278877eb20
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2024-07-07 23:25:20 -0700

    Implement tables api

--- restaurant-webapi/src/ver/v1/tables.rs
+++ restaurant-webapi/src/ver/v1/tables.rs
@@ -1,5 +1,15 @@
-use axum::Router;
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Extension, Json, Router};
+use restaurant::layout;
 
-pub(crate) fn create() -> axum::Router {
-    Router::new()
+use crate::Database;
+
+pub fn create() -> Router {
+    Router::new().route("/", get(get_all))
+}
+
+async fn get_all(Extension(repo): Extension<Database>) -> Result<impl IntoResponse, StatusCode> {
+    layout::get_tables(&repo)
+        .await
+        .map(Json)
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
 }
